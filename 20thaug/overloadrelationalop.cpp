88efcb81c4dd8b2d947cec5ba5cd214b282abaa9
rawoolatharva <125192961+rawoolatharva@users.noreply.github.com>
2024-08-23 09:29:39 +0000

    Added a new folder for 20thaug and added a pgm

---
+++ 20thaug/overloadrelationalop.cpp
@@ -0,0 +1,42 @@
+// Overloading relational operator
+#include <iostream>
+using namespace std;
+
+class Number
+{
+    private:
+        int num;
+    public:
+        friend istream& operator>>(istream&, Number&);
+        int operator>(Number n)
+        {
+            if(num > n.num)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }    
+};
+istream& operator>>(istream &in, Number &n)
+{
+    in>>n.num;
+    return in;
+}
+int main()
+{
+    Number fno, sno;
+    cout<<"\n\tEnter First Number => ";
+    cin>>fno;
+    cout<<"\n\tEnter Second Number => ";
+    cin>>sno;
+    if(fno > sno) //if (fno.operator>(sno))
+        cout<<"\n\tFirst is greater";
+    else if(sno > fno) //if(sno.operator > (fno))
+        cout<<"\n\tSecond Number is greater";
+    else
+        cout<<"\n\tBoth are same";  
+    return 0;      
+}
